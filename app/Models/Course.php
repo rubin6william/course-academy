80fe440ec1e9f23dc8b8348047c5cb5894bbaa50
Rubin William <rubin6william@gmail.com>
2022-01-20 09:05:44 +1100

    Add models for course and enrolments

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * Class Course
+ * @package App\Models
+ */
 class Course extends Model
 {
     use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'description'
+    ];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function enrolments(): BelongsToMany {
+        return $this->belongsToMany(Course::class, 'enrolments');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ * @package App\Models
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -41,4 +46,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function enrolments(): BelongsToMany {
+        return $this->belongsToMany(User::class, 'enrolments');
+    }
 }
